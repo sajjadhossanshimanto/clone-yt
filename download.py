51b7b876075ba854b204b711128fc417fbdca652
shimanto <s.m.sajjadhossanshimanto.c@gmail.com>
2021-12-06 09:53:44 +0000

    download infinity, select best quality, fixed progrssbar

--- download.py
+++ download.py
@@ -5,17 +5,17 @@ import signal
 from typing import Text
 import atexit
 
-from sqlalchemy.sql.coercions import StatementOptionImpl
+
 from firedm.config import Status
 
 from firedm.controller import Controller
 from firedm.cmdview import CmdView
 from firedm.setting import load_setting
 from firedm import FireDM
-from firedm.video import get_media_info, load_extractor_engines
-from firedm.utils import download as direct_download, log, threaded
-from util import load_secssion, thumbnail_folder, table_name, write_secssion
+from firedm.utils import log
+from util import load_secssion, write_secssion, download_thumbnail
 import time
+import util
 
 
 # url='https://youtu.be/nkQz3X8pAAE'
@@ -23,10 +23,16 @@ import time
 # FireDM.main(['firedm', '--dlist', '--interactive', url])
 # FireDM.main(['firedm', '--help'])
 # FireDM.main(['firedm'])
-
-
-
+'''
+/home/kali/.vscode/extensions/ms-python.python-2021.11.1422169775/pythonFiles/lib/jedilsp
+jedi
+parso
+pydantic
+pygls
+typegrud
+'''
 # exit()
+
 
 load_setting()
 controller = Controller(view_class=CmdView)
@@ -39,25 +45,26 @@ atexit.register(cleanup)
 def signal_handler(signum, frame):
     print('\n\nuser interrupt operation, cleanup ...')
     signal.signal(signum, signal.SIG_IGN)  # ignore additional signals
+    util.run=False
     cleanup()
     print('\n\ndone cleanup ...')
-    sys.exit(0)
+    atexit.unregister(cleanup)
+    sys.exit()
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def download_d(d, video_id=''):
+def download_d(d):
     controller._download(d, threaded=False)
+    controller.view.progress=0
     # download thumblain
-    video_id=video_id or d.url.rsplit('=', 1)[-1]
-    video_id+='.jpg'
-    direct_download(d.thumbnail_url, join(thumbnail_folder, video_id), decode=False)
+    download_thumbnail(d)
 
     # delete from downloaded list
     controller.d_map.pop(d.uid, None)
 
 
 sec_file='last_download'
-
+ 
 paused=list(
     filter(
         lambda x:controller.d_map[x].status!=Status.completed,
@@ -69,25 +76,30 @@ if paused:
     
     if len(paused)>1:
         log('Error: multiple paused video found')
+        sys.exit()
     else:
         d=controller.d_map[paused.pop()]
         download_d(d)
-else:
-    secssion=load_secssion(sec_file)
+
+secssion=load_secssion(sec_file)
+while util.run:
     try:
         data=next(secssion)
     except StopIteration:
         log('whole database download completed')
 
     url=f'https://youtu.be/{data.video_id}'
-    controller.process_url(url, threaded=False)
-    loaded=controller.download(video_idx=0, threaded=False)
+    d=controller.process_url(url, threaded=False)[0]
+    d.select_stream(quality='best')
+    loaded=controller.download(d=d, threaded=False)
     
     write_secssion(sec_file, data.index-1)
     log(f'download later secssion saved for {data.index-1}')
     if loaded:
         # start downloading file
         d=controller.download_q.get_nowait()
-        d.select_stream(quality='best')
-        download_d(d, data.video_id)
+        download_d(d)
+    else:
+        log(f'error : {data.index} {data.title}')
     
+    time.sleep(2)
